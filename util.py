735d21f38095a38133adeab27f44aea1921503f7
alexis <alexiswalker@hotmail.com>
2018-05-26 15:45:33 -0300

    filter data

--- util.py
+++ util.py
@@ -1,10 +1,23 @@
 import csv
 
 def load_data():
+    match_list=[]
     with open('results.csv', newline='') as result_file:
         reader = csv.reader(result_file)
+        next(reader) #data includes a first title line
         for row in reader:
-            print(row[2])
+            match_list.append({'date':row[0],'home_team':row[1],'away_team':row[2],'home_score':row[3],'away_score':row[4],'tournament':row[5],'city':row[6],'country':row[7],'neutral':row[8]})
+
+    return match_list
+
+def filter_data(list_to_filter, field, value):
+    return list(filter(lambda match : match[field] == value, list_to_filter))
 
 if __name__ == '__main__':
-    load_data()
+    li = load_data()
+    argentina = filter_data(li, 'tournament', 'FIFA World Cup')
+
+    for m in argentina:
+        print (m)
+
+    print (len(argentina))
