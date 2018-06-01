566d536539103bc20e6855831c9d5fe454dda74f
alexis <alexiswalker@hotmail.com>
2018-05-31 21:25:06 -0300

    setear modelo

--- world_cup.py
+++ world_cup.py
@@ -1,2 +1,20 @@
 from keras.models import Sequential
+from keras.layers import Dense
 model = Sequential()
+
+
+model.add(Dense(units=64, activation='relu', input_dim=3))
+model.add(Dense(units=3, activation='sigmoid'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+
+'''
+model.fit(x_train, y_train, epochs=5, batch_size=32)
+
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+
+print (loss_and_metrics)
+'''
