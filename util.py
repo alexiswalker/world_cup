914dbbca98902d30aaa103a2f73c5a6bc948b902
alexiswalker <alexiswalker@hotmail.com>
2018-06-06 20:00:30 -0300

    load data

--- util.py
+++ util.py
@@ -12,6 +12,16 @@ def load_data():
 
     return match_list
 
+def load_data_positions():
+    world_cups_list=[]
+    with open('positions.csv') as result_file:
+        reader = csv.reader(result_file)
+        next(reader) #data includes a first title line
+        for row in reader:
+            world_cups_list.append({'year':row[0], 'title':row[1],'runner-up':row[2],'third':row[3],'fourth':row[4]})
+
+    return world_cups_list
+
 def different_teams_names(list_to_get_names):
     return list(set([row['home_team'] for row in list_to_get_names] + [row['away_team'] for row in list_to_get_names]))
 
