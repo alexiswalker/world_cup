693a1d4e0fe3a54506537f630ee56fe39fddc9f9
alexiswalker <alexiswalker@hotmail.com>
2018-06-10 12:53:35 -0300

    v0

--- util.py
+++ util.py
@@ -29,28 +29,8 @@ def load_positions_data():
 def different_teams_names(list_to_get_names):
     return list(set([row['home_team'] for row in list_to_get_names] + [row['away_team'] for row in list_to_get_names]))
 
-def positions(positions_list, team):
-    title =  len(filter(lambda world_cup : world_cup['title'] == team, positions_list))
-    runner_up = len(filter(lambda world_cup : world_cup['runner_up'] == team, positions_list))
-    third = len(filter(lambda world_cup : world_cup['third'] == team, positions_list))
-    fourth = len(filter(lambda world_cup : world_cup['fourth'] == team, positions_list))
-
-    return [title*8 + runner_up*4 + third*2 + fourth]
-
-def matches_statistics(matches, team_name):
-    team_matches = list(
-                        filter(lambda match :
-                                    match['home_team'] == team_name
-                                    or
-                                    match['away_team'] == team_name,
-                                matches))
-
-    win = len(filter(lambda match : (match['home_team'] == team_name and match['home_score'] > match['away_score']) or (match['away_team'] == team_name and match['away_score'] > match['home_score']) , team_matches))
-    lose = len(filter(lambda match : (match['home_team'] == team_name and match['home_score'] < match['away_score']) or (match['away_team'] == team_name and match['away_score'] < match['home_score']) , team_matches))
-    tie = len(filter(lambda match : (match['home_team'] == team_name or match['away_team'] == team_name) and ( match['home_score'] == match['away_score']), team_matches))
-    #total = len(filter(lambda match : match['home_team'] == team_name or match['away_team'] == team_name, matches))
-
-    return [win*1.0/(win+lose+tie)]
+def different_years(list_to_get_year):
+    return list(set([row['year'] for row in list_to_get_year]))
 
 def show_data(list_to_show, string_format, number_of_lines=5):
     for row in list_to_show[:number_of_lines]:
--- world_cup.py
+++ world_cup.py
@@ -1,27 +1,13 @@
-import numpy as np
+
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from util import *
+from keras.layers import Dense, Dropout, Activation
 from sklearn.model_selection import train_test_split
-#load data
-#---------
-matches = load_results_data()
-countries_positions = load_positions_data()
-
-filter_matches = list(
-                        filter(lambda match :
-                                    match['tournament'] == 'FIFA World Cup'
-                                    or
-                                    match['tournament'] == 'FIFA World Cup qualification'
-                                ,matches))
-
-different_teams_names = different_teams_names(filter_matches)
+from sklearn.preprocessing import normalize
+from statistics import *
 
 
-
-statistics = dict((team, matches_statistics(filter_matches, team)) for team in different_teams_names)
-scores = dict((team, positions(countries_positions, team)) for team in different_teams_names)
-
+#data for keras
+#--------------
 
 X = []
 y = []
@@ -34,26 +20,41 @@ for match in filter_matches:
 
     X.append(
                 [neutral] +
-                [int(match['year'])] +
-                statistics[match['home_team']] +
-                scores[match['home_team']] +
-                statistics[match['away_team']] +
-                scores[match['away_team']]
+                #[int(match['year'])] +
+                get_statistics(match['home_team']) +
+                get_scores(match['home_team']) +
+                get_year_statistics(str(int(match['year'])), match['home_team']) +
+                get_statistics(match['away_team']) +
+                get_scores(match['away_team']) +
+                get_year_statistics(str(int(match['year'])), match['away_team'])
             )
 
     if int(match['home_score']) > int(match['away_score']):
         y.append(0)
     if int(match['home_score']) < int(match['away_score']):
         y.append(1)
-    if int(match['home_score']) == int(match['away_score']):
-        y.append(2)
 
-X, y = np.array(X), keras.utils.to_categorical(np.array(y), num_classes=3)
+    X.append(
+                [neutral] +
+                #[int(match['year'])] +
+                get_statistics(match['away_team']) +
+                get_scores(match['away_team']) +
+                get_year_statistics(str(int(match['year'])-1), match['away_team']) +
+                get_statistics(match['home_team']) +
+                get_scores(match['home_team']) +
+                get_year_statistics(str(int(match['year'])-1), match['home_team'])
 
+            )
+    if int(match['away_score']) > int(match['home_score']):
+        y.append(0)
+    if int(match['away_score']) < int(match['home_score']):
+        y.append(1)
+    #if int(match['home_score']) == int(match['away_score']):
+    #    y.append(2)
+
+X, y = np.array(X), keras.utils.to_categorical(np.array(y), num_classes=2)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
 
 #create model
 #------------
@@ -61,19 +62,19 @@ model = Sequential()
 
 #neural network
 #--------------
-model.add(Dense(64, activation='relu', input_dim=6))
+model.add(Dense(64, input_dim=17, init='uniform'))
+model.add(Activation('tanh'))
 model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
+model.add(Dense(64, init='uniform'))
+model.add(Activation('tanh'))
 model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
+model.add(Dense(64, init='uniform'))
+model.add(Activation('tanh'))
 model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(3, activation='softmax'))
+model.add(Dense(2, init='uniform'))
+model.add(Activation('softmax'))
 
-#optimizer = keras.optimizers.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False)
+#optimizer = keras.optimizers.SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
 #optimizer = keras.optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
 #optimizer = keras.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0)
@@ -81,15 +82,20 @@ optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
 #optimizer = keras.optimizers.Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
 #optimizer = keras.optimizers.Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
 
-model.compile(loss='categorical_crossentropy',
-              optimizer = optimizer,
-              metrics=['accuracy'])
+model.compile(loss = 'categorical_crossentropy', optimizer = optimizer, metrics=['accuracy'])
 
-model.fit(X_train, y_train)
+model.fit(X_train, y_train, epochs=20, batch_size=128)
 #
-scores = model.evaluate(X_test, y_test)
-print('Test accuracy: %s: %.2f%%' % (model.metrics_names[1], scores[1]*100))
+model_scores = model.evaluate(X_test, y_test)
+print('Test accuracy: %s: %.2f%%' % (model.metrics_names[1], model_scores[1]*100))
 
+X_predict = np.array([
+                            [1] +
+                            get_statistics('Argentina') + get_scores('Argentina') + get_year_statistics('2018','Argentina') +
+                            get_statistics('Paraguay') + get_scores('Paraguay') + get_year_statistics('2018','Paraguay')
+                    ])
+
+print model.predict(X_predict)
 
 #score, acc  = model.evaluate(X_cross_test, y_cross_test)
 #print('Test accuracy: %s: %.2f%%' % (model.metrics_names[1], scores[1]*100))
